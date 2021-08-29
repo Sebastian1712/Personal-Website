fd9df01ad8b07b6768491a957fa65209d79e40ab
Sebastian <sebaoctavio@gmail.com>
2021-08-29 10:54:13 -0400

    modified Title css

--- src/pages/Home/components/Title.tsx
+++ src/pages/Home/components/Title.tsx
@@ -3,12 +3,10 @@ import './Title.css';
 
 function Title() {
     return (
-    <section>
       <div className="titleDiv">
-        <h1>Sebastian Hernandez</h1>
+        <h1>Hi, I'm Sebastian!</h1>
         <p>I’m a knowledge seeker who enjoys communicating about <br></br> computer science, music, philosophy, nature, and much more!</p>
       </div>
-    </section>
     );
   }
   
