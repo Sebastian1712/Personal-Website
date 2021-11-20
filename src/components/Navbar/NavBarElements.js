0a14431babba15284668c09e53fb2554e3873cad
Sebastian <sebaoctavio@gmail.com>
2021-11-20 10:03:17 -0500

    Added on hover animation for nav links

--- src/components/Navbar/NavBarElements.js
+++ src/components/Navbar/NavBarElements.js
@@ -1,33 +1,69 @@
 import styled from 'styled-components'
 import { NavLink as Link} from 'react-router-dom'
 import { FaBars } from 'react-icons/fa'
-
+// rgb(12, 11, 11)
 export const Nav = styled.nav`
     background: #0C0B0B;
     height: 80px;
     display: flex;
     flex-direction: row;
     justify-content: center;
-    align-content: center;
-    align-items: center;
     z-index: 10;
 `
 
 export const NavLink = styled(Link) `
     color: #fff;
     position: relative;
-    display: flex;
-    align-items: center;
+    display: inline-block;
     text-decoration: none;
-    padding: 0 3rem;
-    padding-top: 50px;
+    padding: 0 2rem 0;
     cursor: pointer;
+    
+    /* &:after {
+        content:'';
+        position: absolute;
+        width: 100%;
+        height: 0.175rem;
+        top: 75px;
+        background: linear-gradient(45deg, #a801ff, #0006ff);;
+        transform: scale(0, 1);
+        transition: transform 0.3s ease;
+    }
+    &:hover:after {
+        transform: scale(.7, 1);
+    } */
+    
+    /* Fade in */
+   &::after {
+        top: 23px;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 0.2em;
+        background-color: #7736FF;
+        opacity: 0;
+        transition: opacity 300ms, transform 300ms;
+    }
+
+    &:after {
+        content: '';
+        opacity: 1;
+        transform: scaleX(0);
+        bottom: 0;
+        left: 0;
+        transform-origin: center;
+    }
+
+    &:hover::after,
+    &:focus::after {
+        transform: scaleX(.55);
+    }
 `
 
 export const Bars = styled(FaBars) `
     display: none;
     color: linear-gradient(45deg, #a801ff, #0006ff);
-
     @media screen and (max-width: 768px) {
         display: block;
         position: absolute;
@@ -50,7 +86,6 @@ export const NavBtn = styled.nav `
     display: flex;
     align-items: center;
     margin-right: 24px;
-
     @media screen and (max-width: 768px) {
         display: none;
     }
@@ -65,4 +100,19 @@ export const MainNavLink = styled(Link) `
     padding: 0 1rem;
     padding-top: 50px;
     cursor: pointer;
+    /* &:after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 0.175rem;
+        bottom: 0;
+        left: 0;
+        top: 75px;
+        background: linear-gradient(45deg, #a801ff, #0006ff);;
+        transform: scale(0, 1);
+        transition: transform 0.3s ease;
+    }
+    &:hover::after, &:focus::after {
+        transform: scale(.7, 1);
+    } */
 `
