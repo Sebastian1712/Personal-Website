99418f46128d31a2c1a258edda7884b09af9968d
Sebastian1712 <sebaoctavio@gmail.com>
2021-12-14 13:11:27 -0500

    Added a startup animation to the home page.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './components/Title';
 import MusicAnimation from '../../components/MusicAnimation';
 import Icons from '../../components/IconLinks/Icon';
 
 function Home() {
+  const [animate, setAnimation] = useState("true");
+
     return (
       <div className="homeDiv">
-        <Title/>
+        <Title animate={animate} setAnimation={setAnimation}/>
         <Icons></Icons>
       </div>
     );
