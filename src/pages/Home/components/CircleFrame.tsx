9506587963ad3e8cce1436054246e448fce2498c
Sebastian <sebaoctavio@gmail.com>
2021-12-22 00:14:23 -0500

    Added no repeat to themes.js

--- src/pages/Home/components/CircleFrame.tsx
+++ src/pages/Home/components/CircleFrame.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import styled from 'styled-components';
-import memoji from '../../../public/sebastianIcon.png'
 
 const CircleFrame = styled.div`
     position: flex;
--- src/themes.js
+++ src/themes.js
@@ -23,10 +23,10 @@ export const GlobalStyles = createGlobalStyle `
     }
     .circleFrame {
         ${(props) => (props.theme.purpleColor !== "light") ? 
-        css`background: linear-gradient(180deg, #08C8F6 0%, rgba(8,200,246,1) 0%, rgba(124,136,255,0.72) 100%);
-            image-rendering: url(${memoji});
-            content: url(${memoji});` 
-        : css`background: linear-gradient(180deg, #DBC6FF 31.25%, rgba(249, 197, 209, 0.44) 100%);
+        css`content:  url(${memoji});
+            background: linear-gradient(180deg, #08C8F6 0%, rgba(8,200,246,1) 0%, rgba(124,136,255,0.72) 100%) no-repeat;
+            image-rendering: url(${memoji});` 
+        : css`background: linear-gradient(180deg, #DBC6FF 31.25%, rgba(249, 197, 209, 0.44) 100%) no-repeat;
             image-rendering: url(${memoji});
             content: url(${memoji});`};
     }
