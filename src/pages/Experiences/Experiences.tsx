52beef19500cad69e28d5eba83f8ea9fc32c1adf
Sebastian <sebaoctavio@gmail.com>
2021-12-25 19:10:29 -0700

    removed changes from experiences.tsx

--- src/pages/Experiences/Experiences.tsx
+++ src/pages/Experiences/Experiences.tsx
@@ -1,87 +1,11 @@
 import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { experiencesTransitions, pageTransitions, transitionSettings } from '../../components/Transitions';
+import { pageTransitions, transitionSettings } from '../../components/Transitions';
 import './Experiences.css';
-import Frame from '../../components/Frame/Frame';
-import microsoftLogo from '../../public/microsoftIcon.png';
-import intelLogo from '../../public/newIntelIcon.png';
-import { FrameConfigurations } from '../../components/Frame/FrameConfigurations';
-import { FrameArtifacts } from '../../components/Frame/FrameArtifacts';
-import { Route, Switch, useParams, useRouteMatch } from 'react-router-dom';
-import styled from 'styled-components';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import ExperiencesNav from './ExperiencesNav';
 import Projects from './Projects';
-import { NavLink } from '../../components/Navbar/NavBarElements';
 import Jobs from './Jobs';
-
-// i want to render the job frames (component) from start
-// but, the projects component if click the component
-const experienceOne: FrameConfigurations & FrameArtifacts = {
-  width: 300,
-  height: 150,
-  strokeSize: 3,
-  color: "F7F2EC",
-  icon: microsoftLogo,
-  title: "Microsoft",
-  description: "Software Engineering Intern"
-}
-
-const experienceTwo: FrameConfigurations & FrameArtifacts = {
-  width: 300,
-  height: 150,
-  strokeSize: 3,
-  color: "F7F2EC",
-  icon: microsoftLogo,
-  title: "Microsoft",
-  description: "Explore Intern"
-}
-
-const experienceThree: FrameConfigurations & FrameArtifacts = {
-  width: 300,
-  height: 150,
-  strokeSize: 3,
-  color: "F7F2EC",
-  icon: intelLogo,
-  title: "Intel",
-  description: "Business Ops Intern"
-}
-
-const experiences = [
-  {
-    name: 'jobs',
-    id: '/'
-  },
-  {
-    name: 'projects',
-    id: '/projects'
-  }
-]
-
-const StyledExperiencesNav = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: auto;
-  padding: 2%;
-  gap: 2%;
-  margin-top: -3%;
-
-  @media only screen and (max-width: 500px) {
-    margin-bottom: 4%;
-    }
-`
-
-const StyledExperiencesLink = styled(NavLink)`
-  display: flex;
-  justify-content: center;
-  text-decoration: none;
-  cursor: pointer;
-  font-size: 1.5rem;
-  font-weight: 900;
-
-  @media only screen and (max-width: 500px) {
-        font-size: 1.3rem;
-    }
-`;
 
 function Experiences() {
 
