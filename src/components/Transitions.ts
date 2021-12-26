960add327305717ff961932c3125a5668ec3f63a
Sebastian <sebaoctavio@gmail.com>
2021-12-25 19:08:40 -0700

    added nested routing

--- src/components/Transitions.ts
+++ src/components/Transitions.ts
@@ -17,3 +17,18 @@ export const transitionSettings = {
     type: "easeIn",
     duration: .3
 };
+
+export const experiencesTransitions = {
+    initial: {
+        opacity: 0,
+        x: "-8vw"
+    },
+    visible: {
+        opacity: 1,
+        x: 0
+    },
+    hidden: {
+        opacity: 0,
+        y: "-8vh"
+    }
+};
--- src/pages/Experiences/Experiences.tsx
+++ src/pages/Experiences/Experiences.tsx
@@ -1,15 +1,99 @@
 import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { pageTransitions, transitionSettings } from '../../components/Transitions';
+import { experiencesTransitions, pageTransitions, transitionSettings } from '../../components/Transitions';
 import './Experiences.css';
+import Frame from '../../components/Frame/Frame';
+import microsoftLogo from '../../public/microsoftIcon.png';
+import intelLogo from '../../public/newIntelIcon.png';
+import { FrameConfigurations } from '../../components/Frame/FrameConfigurations';
+import { FrameArtifacts } from '../../components/Frame/FrameArtifacts';
+import { Route, Switch, useParams, useRouteMatch } from 'react-router-dom';
+import styled from 'styled-components';
+import ExperiencesNav from './ExperiencesNav';
+import Projects from './Projects';
+import { NavLink } from '../../components/Navbar/NavBarElements';
+import Jobs from './Jobs';
+
+// i want to render the job frames (component) from start
+// but, the projects component if click the component
+const experienceOne: FrameConfigurations & FrameArtifacts = {
+  width: 300,
+  height: 150,
+  strokeSize: 3,
+  color: "F7F2EC",
+  icon: microsoftLogo,
+  title: "Microsoft",
+  description: "Software Engineering Intern"
+}
+
+const experienceTwo: FrameConfigurations & FrameArtifacts = {
+  width: 300,
+  height: 150,
+  strokeSize: 3,
+  color: "F7F2EC",
+  icon: microsoftLogo,
+  title: "Microsoft",
+  description: "Explore Intern"
+}
+
+const experienceThree: FrameConfigurations & FrameArtifacts = {
+  width: 300,
+  height: 150,
+  strokeSize: 3,
+  color: "F7F2EC",
+  icon: intelLogo,
+  title: "Intel",
+  description: "Business Ops Intern"
+}
+
+const experiences = [
+  {
+    name: 'jobs',
+    id: '/'
+  },
+  {
+    name: 'projects',
+    id: '/projects'
+  }
+]
+
+const StyledExperiencesNav = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  padding: 2%;
+  gap: 2%;
+  margin-top: -3%;
+
+  @media only screen and (max-width: 500px) {
+    margin-bottom: 4%;
+    }
+`
+
+const StyledExperiencesLink = styled(NavLink)`
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+  cursor: pointer;
+  font-size: 1.5rem;
+  font-weight: 900;
+
+  @media only screen and (max-width: 500px) {
+        font-size: 1.3rem;
+    }
+`;
 
 function Experiences() {
+
+    const match = useRouteMatch();
+    console.log("This is the match path " + match.path);
 
     useEffect(() => {
       document.title = "Sebastian | Experiences"
     }, []);
 
     return (
+      
       <motion.div 
       className="experiencesDiv" 
       initial="initial"
@@ -17,11 +101,18 @@ function Experiences() {
       exit="hidden"
       variants={pageTransitions}
       transition={transitionSettings}>
-          <h1>
-            🛠💻 Experiences page under development 🛠💻
-          </h1>
+        <ExperiencesNav/>
+        <Switch>
+            <Route path={`${match.path}/projects`}>
+              <Projects/>
+            </Route>
+            <Route path={match.path}>
+              <Jobs/>
+            </Route>
+        </Switch>
+
       </motion.div>
     );
   }
-  
+
   export default Experiences;
