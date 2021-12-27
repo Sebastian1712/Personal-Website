ca0ee5df6d588b57e5bbd51ad1cf3332026d7936
Sebastian <sebaoctavio@gmail.com>
2021-12-27 12:32:49 -0700

    added a frontend folder

--- src/components/Frame/FrameConfigurations.tsx
+++
@@ -1,6 +0,0 @@
-export type FrameConfigurations = {
-  width: number,
-  height: number,
-  strokeSize: number,
-  color: string
-}
--- src/pages/Experiences/JobsBlog.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const JobsBlog = () => {
-    return (
-        <h1 className="selfDiv">Job Title</h1>
-    );
-}
-
-export default JobsBlog;
